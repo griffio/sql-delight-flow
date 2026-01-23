a0c8ce9e87261176efe654a036eee0470270e45e
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-01-23 17:19:10 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     application
-        id("app.cash.sqldelight") version "2.2.1"
+    id("app.cash.sqldelight") version "2.2.1"
 }
 
 group = "griffio"
